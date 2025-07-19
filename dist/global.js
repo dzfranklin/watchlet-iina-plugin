f9f62c7ef62c181fe0356b86c11a0b1ab538d518
Daniel Franklin <daniel@danielzfranklin.org>
2025-07-19 14:44:06 +0100

    remove dist

--- dist/global.js
+++
@@ -1,6 +0,0 @@
-(function () {
-var $48a6d9d2c45a4470$var$console = iina.console;
-$48a6d9d2c45a4470$var$console.log("Watchlet plugin is running - global.js");
-
-})();
-//# sourceMappingURL=global.js.map
